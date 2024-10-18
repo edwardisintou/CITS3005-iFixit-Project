6fa41563a216fa52870742d423a36d7cb2d37252
Edward Le <leekhoaa@gmail.com>
2024-10-18 09:42:17 +0800

    remove unnecessary test files

--- scripts/test3.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('data/Phone.json') as f:
-    for line in f:
-        phone_data = json.loads(line.strip())
-        if phone_data['Title'] == 'Blackberry DTek 60 Battery Replacement':
-            for tool_data in phone_data.get("Toolbox", []):
-                tool_name = tool_data["Name"]
-                print(tool_name)
--- scripts/test_query1.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open('data/Phone.json') as f:
-    for line in f:
-        phone_data = json.loads(line.strip())
-        if phone_data['Title'] == 'Motorola RAZR Maxx HD Front-Facing Camera Replacement':
-            for step_data in phone_data.get("Steps", []):
-                print(step_data['Order'])
